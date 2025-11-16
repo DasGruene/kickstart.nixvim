dd7834136feb9d60fd482a02f2e12da435279ebc
DasGruene <caspergronnehotmail.com>
2025-11-16 13:06:53 +0100

    adds doxygen support

--- nixvim.nix
+++ nixvim.nix
@@ -396,6 +396,7 @@ in
   # https://nix-community.github.io/nixvim/NeovimOptions/index.html#extraplugins
   extraPlugins = with pkgs.vimPlugins; [
     # NOTE: This is where you would add a vim plugin that is not implemented in Nixvim, also see extraConfigLuaPre below
+    DoxygenToolkit-vim
   ];
 
   # https://nix-community.github.io/nixvim/NeovimOptions/index.html#extraconfigluapost
