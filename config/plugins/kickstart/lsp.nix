760ee949272126757f481ae1b600ae2e2a1e7cf3
DasGruene <caspergronnehotmail.com>
2026-02-27 19:04:06 +0100

    adds rust lsp

--- config/plugins/kickstart/lsp.nix
+++ config/plugins/kickstart/lsp.nix
@@ -88,9 +88,11 @@
       nixd.enable = true;
       pylsp.enable = true;
       zls.enable = true;
-      # rust_analyzer = {
-      #   enable = true;
-      # };
+      rust_analyzer = {
+        enable = true;
+        installCargo = true;
+        installRustc = true;
+      };
       # ...etc. See `https://nix-community.github.io/nixvim/plugins/lsp` for a list of pre-configured LSPs
       #
       # Some languages (like typscript) have entire language plugins that can be useful:
